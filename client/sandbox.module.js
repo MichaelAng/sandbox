80d61f43a32a0be4783d112235d9e251f0cb8c30
Michael Ang <angmerica@gmail.com>
2016-05-07 11:40:08 -0400

    removed sandboxApp and add sandbox module

---
+++ client/sandbox.module.js
@@ -0,0 +1,9 @@
+(function () {
+    'use strict';
+    angular.module('sandbox', [
+        'content.controller',
+        'ngMaterial',
+        'sandbox.router',
+        'sidenav.controller'
+    ]);
+})();
