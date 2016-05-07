a73ba00f0cdfa4ac690ee0009d5458a770f1bfe6
Michael Ang <angmerica@gmail.com>
2016-05-07 11:42:31 -0400

    add router for navigation

---
+++ client/sandbox.router.js
@@ -0,0 +1,34 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('sandbox.router', [
+            'ui.router'
+        ])
+        .config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+    function config($stateProvider, $urlRouterProvider) {
+        // For any unmatched url, redirect to /welcome
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+            .state('home', {
+                abstract: true,
+                views: {
+                    'sidenav': {
+                        templateUrl: '/components/sidenav/sidenav.tmpl.html',
+                        controller: 'SideNavController'
+                    },
+                    '' : {
+                        templateUrl: '/components/content/content.tmpl.html',
+                        controller: 'ContentController'
+                    }
+                }
+            })
+            .state('home.welcome', {
+                url: '/'
+            });
+    }
+
+})();
