3e93f00e06464c0672c6e1af9a02c5fb08473a41
Michael Ang <angmerica@gmail.com>
2016-05-13 09:45:20 -0400

    create skeleton of sidenav

--- client/components/sidenav/sidenav.controller.js
+++ client/components/sidenav/sidenav.controller.js
@@ -4,11 +4,5 @@
     angular
         .module('sidenav.controller', [])
         .controller('SideNavController', function ($scope, $timeout, $mdSidenav, $log) {
-            $scope.close = function () {
-            $mdSidenav('left').close()
-                .then(function () {
-                    $log.debug("close LEFT is done");
-                });
-            };
         });
 })();
