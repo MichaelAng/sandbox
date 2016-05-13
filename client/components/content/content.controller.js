9c67482240816b2a96b89da1910b442f9b92530f
Michael Ang <angmerica@gmail.com>
2016-05-13 09:45:06 -0400

    create skeleton of content

--- client/components/content/content.controller.js
+++ client/components/content/content.controller.js
@@ -4,12 +4,5 @@
     angular
         .module('content.controller', [])
         .controller('ContentController', function ($scope, $timeout, $mdSidenav, $log) {
-            $scope.toggleLeft = function () {
-            $mdSidenav('left')
-                .toggle()
-                .then(function () {
-
-                 });
-            };
         });
 })();
