2fc1de4977b4828f25b105c74a109eb5541eb5f3
Michael Ang <angmerica@gmail.com>
2016-06-04 21:27:21 -0400

    updated routes

--- server/routes.js
+++ server/routes.js
@@ -6,10 +6,4 @@ module.exports = function(app) {
 
   // Insert routes below
   app.use('/api/task', require('./api/task'));
-
-  // All other routes should redirect to the index.html
-  app.route('/*')
-    .get(function(req, res) {
-      res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
-    });
 };
