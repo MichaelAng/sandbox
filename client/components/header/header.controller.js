3b31a3719659304ee4919a7b8c8abe4ba26f06fb
Michael Ang <angmerica@gmail.com>
2016-06-08 19:45:25 -0400

    create filter option

--- client/components/header/header.controller.js
+++ client/components/header/header.controller.js
@@ -3,7 +3,18 @@
 
     angular
         .module('header.module')
-        .controller('HeaderController', function () {
-        });
+        .controller('HeaderController', HeaderController);
+
+    HeaderController.$inject = ['Task'];
+    function HeaderController (Task) {
+        var vm = this;
+        vm.search = { name: '' };
+
+        vm.filterTaskListByName = filterTaskListByName;
+
+        function filterTaskListByName() {
+            Task.filterTaskList(vm.search);
+        }
+    }
 })();
 
